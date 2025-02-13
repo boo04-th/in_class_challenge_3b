d147c9fb86dc125edae6a8c2a631044672bb2d8d
Hient <hient@usf.edu>
2025-02-13 16:01:42 -0500

    Task 6: Inventory Stock Management

---
+++ cc_example.js
@@ -0,0 +1,35 @@
+// Task 5
+// let orders = [
+//     {id: 101, customer:"Alice", total:300},
+//     {id: 102, customer:"Bob", total: 450},
+//     {id: 103, customer:"Charlie", total: 200}
+// ];
+
+// function findOrder(orders, orderId){
+//     return orders.find(order => order.id === orderId);
+// }
+
+// console.log (findOrder(orders,102));
+
+//Task 6
+let inventory ={
+    items: [],
+    addItem(name, quantity){
+        this.items.push({name, quantity});
+    }
+    removeLastestItem(){
+        this.items.pop();
+    },
+    removeFirstItem(){
+        this.items.shift();
+    },
+}
+
+inventory.addItem("Monitor",5);
+inventory.addItem("Keyboard",10);
+inventory.addItem("Mouse",3);
+console.log(JSON.stringify(inventory.items));
+inventory.removeLastestItem();
+console.log(JSON.stringify(inventory.items));
+inventory.removeFirstItem();
+console.log(JSON.stringify(inventory.items));
